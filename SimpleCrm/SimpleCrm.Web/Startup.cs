b2c97dd9a67badf41c31e1673532b41a6a790f90
MadHattressWatson <MadhattressWatson@gmail.com>
2022-01-31 15:05:34 -0600

    Step005

--- SimpleCrm/SimpleCrm.Web/Startup.cs
+++ SimpleCrm/SimpleCrm.Web/Startup.cs
@@ -4,46 +4,40 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
 
 namespace SimpleCrm.Web
 {
     public class Startup
     {
-        private readonly IConfiguration configuration;
+        private readonly IConfiguration _configuration;
 
         public Startup(IConfiguration configuration)
         {
-            this.configuration = configuration;
+            this._configuration = configuration;
         }
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, ConfigurationGreeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                   var greeting = configuration["Greeting"];
-                    await context.Response.WriteAsync(greeting);
-                });
+                var message = greeter.GetGreeting();
+                await context.Response.WriteAsync(message);
             });
         }
     }
---
+++ SimpleCrm/SimpleCrm/ConfigurationGreeter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace SimpleCrm
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        public IConfiguration Configuration { get; }
+        public ConfigurationGreeter(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public string GetGreeting()
+        {
+            return Configuration["Greeting"];
+        }
+    }
+}
