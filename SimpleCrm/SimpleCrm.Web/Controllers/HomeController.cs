4ee28a78aa42ed0d3fbd324d8c6ab4c6fcd77ede
MadHattressWatson <MadhattressWatson@gmail.com>
2022-02-02 17:09:41 -0600

    Step012

--- SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -2,19 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Linq;
+using SimpleCrm.Web.Models;
 
 namespace SimpleCrm.Web.Controllers
 {
-    [Description()]
-    public class HomeControler
-    { 
-        [Description("")]
-        public int MyProperty { get; set; }
-        public string Index()
+    public class HomeControler : Controller
+    {
+        public IActionResult Index()
         {
-            return "Hello from a Controller ";
+          var model = new CustomerModel
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                PhoneNumber = "555-555-1234"
+            };
+            return new ObjectResult(model);
+              
         }
     }
-};
+}
