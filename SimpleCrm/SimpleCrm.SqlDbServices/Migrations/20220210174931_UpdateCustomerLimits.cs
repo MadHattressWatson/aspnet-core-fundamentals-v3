aab9a596d2b0a6414f56cd53fe685005813c670e
MadHattressWatson <MadhattressWatson@gmail.com>
2022-02-10 11:53:42 -0600

    Step23

---
+++ SimpleCrm/SimpleCrm.SqlDbServices/Migrations/20220210174931_UpdateCustomerLimits.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleCrm.SqlDbServices.Migrations
+{
+    public partial class UpdateCustomerLimits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
