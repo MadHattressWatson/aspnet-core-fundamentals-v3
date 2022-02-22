83c83938e38041b14a8a9d6cdb8ef281320a3622
MadHattressWatson <MadhattressWatson@gmail.com>
2022-02-22 17:35:33 -0600

    Step 54 customer list table on a card, with a max content width

--- SimpleCrm/simple-crm-cli/src/app/customer--routing/customer.module.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer--routing/customer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { CommonModule } from '@angular/common;
+import { MatTableModule} from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 
@@ -9,6 +10,8 @@ import { CustomerListPageComponent } from './customer-list-page/customer-list-pa
   declarations: [CustomerListPageComponent],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatTableModule,
     CustomerRoutingModule
   ]
 })
--- SimpleCrm/simple-crm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer.model';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'crm-customer-list-page',
@@ -7,18 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListPageComponent implements OnInit {
 
-  customers: Customer[];
+  customers: Customer[] = [
+    {
+      customerId: 1,
+      firstName: 'John',
+      lastName: 'Smith',
+      phoneNumber: '314-555-1234',
+      emailAddress: 'john@nexulacademy.com',
+      statusCode: 'Prospect',
+      preferredContactMethod: 'phone',
+      lastContactDate: new Date().toISOString()
+    },
+    {
+      customerId: 2,
+      firstName: 'Tory',
+      lastName: 'Amos',
+      phoneNumber: '314-555-9873',
+      emailAddress: 'tory@example.com',
+      statusCode: 'Prospect',
+      preferredContactMethod: 'email',
+      lastContactDate: new Date().toISOString()
+    }
+  ];
+  dataSource!: MatTableDataSource<Customer>; // The ! tells Angular you know it may be used before it is set.  Try it without to see the error
+  displayColumns = ['name', 'phone', 'email', 'status'];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.customers = [
-      {
-        customerId: 1,
-        lastContactDate: '2020-01-01',
-        preferredContactMethod: 'email'
-      }
-    ];
+    this.dataSource = new MatTableDataSource(this.customers);
+    }
   }
-
-}
