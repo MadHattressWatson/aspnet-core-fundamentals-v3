8257e31921fe655d39151bb619a58ed6c6ca7f12
MadHattressWatson <MadhattressWatson@gmail.com>
2022-02-22 23:54:18 -0600

    Step 56: mock customer service created

--- SimpleCrm/simple-crm-cli/src/app/customer/customer.module.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer.module.ts
@@ -5,6 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 import { CustomerRoutingModule } from './customer-routing.module';
 import  { HttpClientModule } from '@angular/common/http';
+import { CustomerService } from './customer.service';
+import { CustomerMockService } from './customer-mock.service';
 
 @NgModule({
   declarations: [CustomerListPageComponent],
@@ -13,6 +15,12 @@ import  { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     MatTableModule,
     CustomerRoutingModule
+],
+  providers: [
+    {
+      provider: CustomerService,
+      useClass: CustomerMockService
+    }
   ]
 })
 
