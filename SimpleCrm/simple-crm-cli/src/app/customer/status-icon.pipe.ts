4eaf4919b9f8caaee2bca46aa8a0cfd0c5e5e27d
MadHattressWatson <MadhattressWatson@gmail.com>
2022-03-01 01:32:01 -0600

    69: fix service tests

--- SimpleCrm/simple-crm-cli/src/app/customer/status-icon.pipe.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/status-icon.pipe.ts
@@ -1,19 +1,49 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { StatusIconPipe } from './status-icon.pipe';
 
-@Pipe({
-  name: 'statusIcon'
-})
-export class StatusIconPipe implements PipeTransform {
+describe('StatusIconPipe', () => {
+  it('create an instance', ()=>{
+    const pipe = new StatusIconPipe();
+    expect(pipe).toBeTruthy();
+  });
+  it('Prospect should result in online', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('Prospect');
+    expect(x).toEqual('online');
+  });
+  it('prospect (lowercase) should result in online', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('prospect');
+    expect(x).toEqual('online');
+  });
+  it('prOspEct (mixed case) should result in online', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('prOspEct');
+    expect(x).toEqual('online');
+  });
+  it('Purchased should result in money', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('Purchased');
+    expect(x).toEqual('money');
+  });
+  it('purchased (lowercase) should result in money', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('purchased');
+    expect(x).toEqual('money');
+  });
+  it('pUrchased (mixed case) should result in money', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('pUrchased');
+    expect(x).toEqual('money');
+  });
+  it('empty string should result in fallback value', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform('');
+    expect(x).toEqual('users');
+  });
+  it('null should result in fallback value', () => {
+    const pipe = new StatusIconPipe();
+    const x = pipe.transform(null);
+    expect(x).toEqual('users');
+  });
+});
 
-  transform(value: string, ...args: unknown[]): unknown {
-    value = value ||'';
-    if ( value.search(/prospectr/i) ===0) {
-      return 'online'
-    }
-    if ( value.search(/purchased/i) === 0){
-      return 'money';
-    }
-    return 'users'
-  }
-
-}
