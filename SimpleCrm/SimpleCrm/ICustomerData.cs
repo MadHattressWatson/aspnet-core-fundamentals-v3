56b7c4f8bd3cce82294335cfd494eab2dc08026b
MadHattressWatson <MadhattressWatson@gmail.com>
2022-03-03 15:40:23 -0600

    101: add batch save support

--- SimpleCrm/SimpleCrm/ICustomerData.cs
+++ SimpleCrm/SimpleCrm/ICustomerData.cs
@@ -9,5 +9,11 @@ namespace SimpleCrm
         Customer Get(int id);
         void Add(Customer customer);
         void Update(Customer customer);
+        /// <summary>
+        /// Saves changes to new or modified customers.
+        /// </summary>
+        void Commit();  // <- NEW!
     }
 }
+
+  
