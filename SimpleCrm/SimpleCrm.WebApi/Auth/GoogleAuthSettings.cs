4136f7dd6b3882e99c77ec642889c6c0711d935e
MadHattressWatson <MadhattressWatson@gmail.com>
2022-03-16 06:02:44 -0500

    114: add google and microsoft providers

---
+++ SimpleCrm/SimpleCrm.WebApi/Auth/GoogleAuthSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleCrm.WebApi.Auth
+{
+    public class GoogleAuthSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
+    
